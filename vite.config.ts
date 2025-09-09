480b194de46f34dba7ef26f6bbd5dd1ed4c80659
NEKOgrile <willemcornil@gmail.com>
2025-09-09 18:09:58 +0200

    maj test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/project/', // ← IMPORTANT pour GitHub Pages
+
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
