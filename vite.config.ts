5479b1741edb4ad082cde38910451ac377c9751e
NEKOgrile <98735979+NEKOgrile@users.noreply.github.com>
2025-09-11 17:41:12 +0200

    Correction de la configuration de déploiement

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/project/', // ← c'est bon si ton repo s'appelle "project"
+    base: './', // ← chemin relatif pour le déploiement
     plugins: [react()],
     optimizeDeps: {
         exclude: ['lucide-react'],
