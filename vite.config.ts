7e293bccf13488e2a0447c3c3215a3d0a480ccb0
NEKOgrile <98735979+NEKOgrile@users.noreply.github.com>
2025-09-11 18:41:24 +0200

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './', // ← c'est bon si ton repo s'appelle "project"
-    plugins: [react()],
-    optimizeDeps: {
-        exclude: ['lucide-react'],
-    },
-});
+  base: '/project/',   // ⚠️ obligatoire car ton site est dans /project/
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+})
